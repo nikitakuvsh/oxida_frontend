5b2fc1934fa4578d043f6dcfe77d59f1c9401f8c
nikitakuvsh <nikita_kuvshinnikov@inbox.ru>
2026-03-28 19:18:53 +0300

    add header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './styles.css';
+import Header from './components/Header/Header';
+import Profile from './components/Profile/Profile';
+import Tasks from './components/Tasks/Tasks';
 
 function App() {
   return (
     <div className="App">
+      <Header />
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,21 @@ import './Header.css';
 export default function Header(){
     return (
         <header className='header'>
-            <div className='header__content'></div>
+            <div className='header__content'>
+                <div className='header__logo'>
+                    <h2 className='header__logo-text'>OXIDA</h2>
+                </div>
+
+                <div className='header__nav'>
+                    <nav>
+                        <a className='header__nav-list'>Главная</a>
+                        <a className='header__nav-list'>Мои задачи</a>
+                        <a className='header__nav-list'>Мой профиль</a>
+                    </nav>
+
+                    <p className='header__money'>Баланс: 50000Р</p>
+                </div>
+            </div>
         </header>
     );
 }
